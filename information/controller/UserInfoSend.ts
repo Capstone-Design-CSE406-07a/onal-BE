0de74a8896c07b683e0158bded05352f50ac9118
5eungwon <kimseung0630@naver.com>
2026-04-14 18:57:10 +0900

    initial setting

--- information/controller/UserInfoSend.ts
+++ information/controller/UserInfoSend.ts
@@ -1,21 +1,20 @@
 import { Request, Response } from 'express';
 import User from '../interface/User';
 
-
+// const newUser = await User.create({
+//             id: 'user1',
+//             password: '123',
+//             name: '홍길동',
+//             onboarding: false,
+//             sensivity: 'high',
+//             activity_time: 'evening',
+//             favorite_place: ['강남', '역삼']
+//         });
 
 
 export const UserInfoSend = async (req: Request, res: Response) => {
     try {
-        // const newUser = await User.create(req.body);
-        const newUser = await User.create({
-            id: 'user1',
-            password: '123',
-            name: '홍길동',
-            onboarding: false,
-            sensivity: 'high',
-            activity_time: 'evening',
-            favorite_place: ['강남', '역삼']
-        });
+        const newUser = await User.create(req.body);
         res.status(201).json(newUser);
     } catch (error) {
         res.status(500).json({ message: "서버 에러" });
