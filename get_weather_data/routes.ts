c23cb1e5216c04ec9cd420316f2b5dd0cd8cc0d6
5eungwon <kimseung0630@naver.com>
2026-04-16 01:58:27 +0900

    라우터 수정

--- get_weather_data/routes.ts
+++ get_weather_data/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { Get_Temperture_Wind_Data } from './controller/Get_Temperture_Wind_Data';
-
+import { Get_Temperture_Wind_Data } from './temperture_wind/controller/Get_Temperture_Wind_Data';
+import { Get_Pm_Data } from './pm/controller/Get_Pm_Data';
 const router = Router();
 
+
+router.get('/pm',Get_Pm_Data)
 router.get('/temperture_wind',Get_Temperture_Wind_Data)
 
 
